030be850eabe3f219b6a11a4f9db69065d4b1714
Vadym Borodin <borodean@gmail.com>
2021-12-26 16:25:32 +0200

    Reorder `options.go`

--- options.go
+++ options.go
@@ -1,24 +1,24 @@
 package asciitree
 
+type sortOptions struct {
+	dirsFirst bool
+}
+
 // SortOption represents an option that can be provided to the Sort method.
 type SortOption interface {
 	apply(*sortOptions)
 }
 
-type sortOptions struct {
-	dirsFirst bool
-}
-
 type dirsFirstOption bool
+
+func (d dirsFirstOption) apply(opts *sortOptions) {
+	opts.dirsFirst = bool(d)
+}
 
 // WithDirsFirst is an option that makes the Sort method order directories
 // before leaves.
 func WithDirsFirst(value bool) SortOption {
 	return dirsFirstOption(value)
-}
-
-func (d dirsFirstOption) apply(opts *sortOptions) {
-	opts.dirsFirst = bool(d)
 }
 
 func newSortOptions(opts ...SortOption) sortOptions {
